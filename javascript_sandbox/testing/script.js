49ab7b9997ef204ff628770bf1782fd1607cbeca
Kevin <kevinalexanderduran8@gmail.com>
2022-03-31 18:09:08 -0700

    test

---
+++ javascript_sandbox/testing/script.js
@@ -0,0 +1,12 @@
+function Book(title,author,pages,haveRead){
+    this.title=title;
+    this.author=author;
+    this.pages=pages;
+    this.haveRead = haveRead;
+    this.info = function(){
+        return (this.title +" by "+ this.author+", "+this.pages)
+    }
+}
+
+const test = new Book('lord', 'mama', 2343, true)
+console.log(test.info());
