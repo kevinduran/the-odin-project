f9c424a3b983881f5058878093db0dc05a3d14f6
Kevin <kevinalexanderduran8@gmail.com>
2022-04-05 03:36:37 -0700

    testing

--- javascript_sandbox/project_library/script.js
+++ javascript_sandbox/project_library/script.js
@@ -11,18 +11,55 @@ closeModalButton.addEventListener('click',()=>{
     formModal.classList.remove('formModalActive')
 })
 
+//------------------------BAD PRACTICE ------------------------------
+// const addBookButton = document.querySelector('.addBookButton')
+// const bookContainer = document.querySelector('.bookContainer')
+// addBookButton.addEventListener('click',()=>{
+//     var test = document.querySelector('.bookTitleInput').value;
+//     var test2 = document.createElement('p');
+//     test2.textContent= test;
+//     bookContainer.appendChild(test2)
+//     const formModal = document.querySelector('.formModal')
+//     formModal.classList.remove('formModalActive')
+//     document.querySelector('.bookTitleInput').value = ''
+// })
 
-const addBookButton = document.querySelector('.addBookButton')
-const bookContainer = document.querySelector('.bookContainer')
-addBookButton.addEventListener('click',()=>{
-    var test = document.querySelector('.bookTitleInput').value;
-    var test2 = document.createElement('p');
-    test2.textContent= test;
-    bookContainer.appendChild(test2)
-    const formModal = document.querySelector('.formModal')
-    formModal.classList.remove('formModalActive')
-    document.querySelector('.bookTitleInput').value = ''
-})
+
+
+let myLibrary = [];
+
+function Book(title,author,pages) {
+    this.title=title;
+    this.author=author;
+    this.pages=pages;
+}
+
+
+function addBookToLibrary(){
+    var test = new Book('sdf','sdfaa',4)
+    myLibrary.push(test)
+    myLibrary.push(test)
+    console.log(myLibrary)
+}
+addBookToLibrary()
+
+
+var bookContainer= document.querySelector('.bookContainer');
+    
+
+function displayBooks(){
+    
+    myLibrary.map((x)=>{
+        var p = document.createElement('p');
+        p.textContent= x.title;
+        console.log(p)
+        bookContainer.appendChild(p);
+    })
+}
+
+
+displayBooks()
+
 
 
 
@@ -32,5 +69,7 @@ addBookButton.addEventListener('click',()=>{
 // steps
     //button click opens up modal
     //modal form gets data
-        //data creates dom card
+        //data creates dom card by using the Book constructor
+        //use addBookToLibrary function to add info to array
+            //we will loop through array using map to display to the dom 
     //modal closes on button click
