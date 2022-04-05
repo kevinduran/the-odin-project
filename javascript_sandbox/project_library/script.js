9a7dac9d00f7760ad8714c7c49cc932b24ec7ae6
Kevin <kevinalexanderduran8@gmail.com>
2022-04-04 18:17:37 -0700

    testing

--- javascript_sandbox/project_library/script.js
+++ javascript_sandbox/project_library/script.js
@@ -12,7 +12,25 @@ closeModalButton.addEventListener('click',()=>{
 })
 
 
+const addBookButton = document.querySelector('.addBookButton')
+const bookContainer = document.querySelector('.bookContainer')
+addBookButton.addEventListener('click',()=>{
+    var test = document.querySelector('.bookTitleInput').value;
+    var test2 = document.createElement('p');
+    test2.textContent= test;
+    bookContainer.appendChild(test2)
+    const formModal = document.querySelector('.formModal')
+    formModal.classList.remove('formModalActive')
+    document.querySelector('.bookTitleInput').value = ''
+})
+
+
+
+
+
+
 // steps
     //button click opens up modal
     //modal form gets data
+        //data creates dom card
     //modal closes on button click
