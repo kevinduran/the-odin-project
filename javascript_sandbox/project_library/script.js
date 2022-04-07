8ea12c47f1eca4ab46a8a900c6cb46cf897e1828
Kevin <kevinalexanderduran8@gmail.com>
2022-04-07 16:17:07 -0700

    test

--- javascript_sandbox/project_library/script.js
+++ javascript_sandbox/project_library/script.js
@@ -16,29 +16,46 @@ closeModalButton.addEventListener('click',()=>{
 
 addBookButton.addEventListener('click',()=>{
     var bookTitle = document.querySelector('.bookTitleInput').value;
-    addBookToLibrary(bookTitle)
+    var bookAuthor = document.querySelector('.bookAuthorInput').value;
+    var bookPages = document.querySelector('.bookPagesInput').value;
+    addBookToLibrary(bookTitle,bookAuthor,bookPages)
     const formModal = document.querySelector('.formModal')
     formModal.classList.remove('formModalActive')
     document.querySelector('.bookTitleInput').value = ''
-    // displayBooks()
+    document.querySelector('.bookAuthorInput').value = ''
+    document.querySelector('.bookPagesInput').value = ''
+   
 })
 
 function addBookToLibrary(title,author,pages){
     var newBook = new Book(title,author,pages)
     myLibrary.push(newBook)
+    displayBooks(newBook)
 }
 
-function displayBooks(){
-    
-    myLibrary.map((x)=>{
-        var myDiv = document.createElement('div');
-        myDiv.classList.add('book');
-        myDiv.textContent= x.title;
-        bookContainer.appendChild(myDiv);
-    })
-    //the issue is we are mapping through every unit and displaying it every time we click the button
-    //we need to target the last value of the array becasue we pushed it...
-    //target last value and THEN bookContainer.appendChild(mydiv)
+function displayBooks(book){        
+    var myDiv = document.createElement('div')
+    myDiv.classList.add('book');
+    const randomColor = Math.floor(Math.random()*16777215).toString(16);
+    const style = {
+    boxShadow: '0 0 80px #'+randomColor+', 0 0 32px #b678b078',
+    background: 'linear-gradient(135deg, #'+randomColor+' 0%, #f4f4f4 100%)'
+    }
+    Object.assign(myDiv.style,style)
+
+
+    var title = document.createElement('h1');
+    title.textContent = book.title
+    var author = document.createElement('p');
+    author.textContent = book.author;
+    var pages = document.createElement('p');
+    pages.textContent = book.pages
+    myDiv.appendChild(title)
+    myDiv.appendChild(author)
+    myDiv.appendChild(pages)
+
+    bookContainer.appendChild(myDiv); 
+    console.log(myLibrary)  
 }
 
 function Book(title,author,pages) {
