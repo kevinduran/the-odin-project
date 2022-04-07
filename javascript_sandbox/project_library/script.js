dadc2b28a1ec8427482309a542250b2eabd98d17
Kevin <kevinalexanderduran8@gmail.com>
2022-04-07 16:38:51 -0700

    refactoring

--- javascript_sandbox/project_library/script.js
+++ javascript_sandbox/project_library/script.js
@@ -2,8 +2,13 @@ const openModalButton = document.querySelector('.openModalButton')
 const closeModalButton = document.querySelector('.closeModalButton')
 const addBookButton = document.querySelector('.addBookButton')
 const bookContainer = document.querySelector('.bookContainer')
-let myLibrary = [];
 
+//-----myLibrary will hold all book data
+let myLibrary = [];
+//-----
+
+
+//-----open and close modal
 openModalButton.addEventListener('click',()=>{
     const formModal = document.querySelector('.formModal')
     formModal.classList.add('formModalActive')
@@ -13,17 +18,31 @@ closeModalButton.addEventListener('click',()=>{
     const formModal = document.querySelector('.formModal')
     formModal.classList.remove('formModalActive')
 })
+//-----
+
+
+
+
 
 addBookButton.addEventListener('click',()=>{
+    //-----collect all input data and pass it to addBook to library function
     var bookTitle = document.querySelector('.bookTitleInput').value;
     var bookAuthor = document.querySelector('.bookAuthorInput').value;
     var bookPages = document.querySelector('.bookPagesInput').value;
     addBookToLibrary(bookTitle,bookAuthor,bookPages)
+    //-----
+
+    //-----close modal
     const formModal = document.querySelector('.formModal')
     formModal.classList.remove('formModalActive')
+    //-----
+
+    //-----clear inputs
     document.querySelector('.bookTitleInput').value = ''
     document.querySelector('.bookAuthorInput').value = ''
     document.querySelector('.bookPagesInput').value = ''
+    //-----
+    
    
 })
 
@@ -33,33 +52,46 @@ function addBookToLibrary(title,author,pages){
     displayBooks(newBook)
 }
 
-function displayBooks(book){        
+function displayBooks(book){   
+    //-----create div element and give it a class book     
     var myDiv = document.createElement('div')
     myDiv.classList.add('book');
+    //-----  
+
+
+    //-----adding random color to each book
     const randomColor = Math.floor(Math.random()*16777215).toString(16);
     const style = {
     boxShadow: '0 0 80px #'+randomColor+', 0 0 32px #b678b078',
     background: 'linear-gradient(135deg, #'+randomColor+' 0%, #f4f4f4 100%)'
     }
     Object.assign(myDiv.style,style)
+    //-----
 
-
-    var title = document.createElement('h1');
+    //-----creating text elements and append them to book
+    var title = document.createElement('h2');
     title.textContent = book.title
     var author = document.createElement('p');
     author.textContent = book.author;
     var pages = document.createElement('p');
     pages.textContent = book.pages + ' pg.'
+
     myDiv.appendChild(title)
     myDiv.appendChild(author)
     myDiv.appendChild(pages)
+    //-----
 
+    //-----appenging book to book container
     bookContainer.appendChild(myDiv); 
-    console.log(myLibrary)  
+    //-----
+    
 }
 
+
+//-----book constructor
 function Book(title,author,pages) {
     this.title=title;
     this.author=author;
     this.pages=pages;
 }
+//-----
