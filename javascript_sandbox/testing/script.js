7b243158dba27202de37a9946760c4b3b4d856fa
Kevin <kevinalexanderduran8@gmail.com>
2022-07-20 19:23:40 -0700

    async/await refactoring

--- javascript_sandbox/testing/script.js
+++ javascript_sandbox/testing/script.js
@@ -1,15 +1,11 @@
 const img = document.querySelector('img');
 
-const call_api = () => {
-    fetch('https://api.giphy.com/v1/gifs/translate?api_key=YVKIj3DyD3OL3TGN7FeVATealNxUdDwm&s=cats', {
-        mode: 'cors'
-        //above is small fix for CORS
-      })
-    .then(res=>res.json())
-    .then(data=>img.src=data.data.images.fixed_height.url)
-    .catch(err=>console.error('error!!!'))
+ async function getCats() {
+    const response = await fetch('https://api.giphy.com/v1/gifs/translate?api_key=YVKIj3DyD3OL3TGN7FeVATealNxUdDwm&s=cats', {mode: 'cors'});
+    const catData = await response.json(); 
+    img.src = catData.data.images.original.url;
 }
 
-call_api();
+getCats();
 
 
