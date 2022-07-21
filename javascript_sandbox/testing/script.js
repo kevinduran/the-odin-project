b33a462702ac99acc199769f93b29753e7794455
Kevin <kevinalexanderduran8@gmail.com>
2022-07-20 18:39:55 -0700

    test

--- javascript_sandbox/testing/script.js
+++ javascript_sandbox/testing/script.js
@@ -1,8 +1,15 @@
+const img = document.querySelector('img');
+
 const call_api = () => {
-    fetch('https://api.giphy.com/v1/gifs/translate?api_key=YVKIj3DyD3OL3TGN7FeVATealNxUdDwm&s=cats')
+    fetch('https://api.giphy.com/v1/gifs/translate?api_key=YVKIj3DyD3OL3TGN7FeVATealNxUdDwm&s=cats', {
+        mode: 'cors'
+        //above is small fix for CORS
+      })
     .then(res=>res.json())
-    .then(data=>console.log(data))
+    .then(data=>img.src=data.data.images.original.url)
     .catch(err=>console.error('error!!!'))
 }
 
 call_api();
+
+
