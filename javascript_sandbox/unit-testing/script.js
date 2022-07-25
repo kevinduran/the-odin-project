cd9a06ab3ce94ef69b5dff88cac28e82f7a658ac
Kevin <kevinalexanderduran8@gmail.com>
2022-07-24 18:37:32 -0700

    testing

---
+++ javascript_sandbox/unit-testing/script.js
@@ -0,0 +1,13 @@
+if(orderTotal({
+    item:[
+        {name:"Dragon food",price:8},
+        {name:"Dragon cage(small)", price:800}
+    ]
+}) !== 808) {
+    throw new Error('Check fail: Happy path')
+}
+
+function orderTotal(order){
+    return order.item.reduce((prev, cur)=> cur.price+prev,0)
+}
+
