b9a11ef7e832826ffe47123fca85dc3b21dcd6e1
Kevin <kevinalexanderduran8@gmail.com>
2022-08-20 15:17:41 -0700

    test

--- javascript_sandbox/unit-testing/script.js
+++ javascript_sandbox/unit-testing/script.js
@@ -1,38 +1,44 @@
-if(orderTotal({
-    item:[
-        {name:"Dragon food",price:8},
-        {name:"Dragon cage(small)", price:800}
-    ]
-}) !== 808) {
+if (orderTotal({
+        item: [{
+                name: "Dragon food",
+                price: 8
+            },
+            {
+                name: "Dragon cage(small)",
+                price: 800
+            }
+        ]
+    }) !== 808) {
     throw new Error('Check fail: Happy path')
 }
 
-function orderTotal(order){
-    return order.item.reduce((prev, cur)=> cur.price+prev,0)
+function orderTotal(order) {
+    return order.item.reduce((prev, cur) => cur.price + prev, 0)
 }
 
 //---------------------------------------
 
 
-function Person(name,age){
-   this.name = name;
-   this.age = age;
+function Person(name, age) {
+    this.name = name;
+    this.age = age;
 }
 
-const Joe = new Person('joe',32)
+const Joe = new Person('joe', 32)
 
-Person.prototype.sayName = function() {
-    console.log('congrats '+this.name)
+Person.prototype.sayName = function () {
+    console.log('congrats ' + this.name)
 }
 
 Joe.sayName()
-const Kevin = new Person('kevin',31);
+const Kevin = new Person('kevin', 31);
 Kevin.sayName()
 console.log('test');
 console.log(': hello, I am Kevin')
 
 
-function Woman (age,name) {
+function Woman(age, name) {
     this.age = age,
-    this.name = name
+        this.name = name
 }
+console.log('tesd')
