b645c56ab3a0738c87db03045225fa023dd4539a
Kevin <kevinalexanderduran8@gmail.com>
2022-09-16 18:41:50 -0700

    test

--- node_sandbox/app.js
+++ node_sandbox/app.js
@@ -1,1 +1,15 @@
-console.log('hello friend')
+const http = require('http');
+
+const HOSTNAME= process.env.HOSTNAME || 'localhost';
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer((request,response)=>{
+    response.statusCode=200;
+    response.setHeader('content-type','text/plain');
+    response.end('hello friend')
+
+})
+
+server.listen(PORT,HOSTNAME,()=>{
+    console.log(`server running at http://${HOSTNAME}:${PORT}/`)
+})
