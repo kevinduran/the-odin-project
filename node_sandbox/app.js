5ad4e6aeb788fcb95da3c135dfc2a2cc51cec43a
Kevin <kevinalexanderduran8@gmail.com>
2022-09-16 18:48:43 -0700

    test

--- node_sandbox/app.js
+++ node_sandbox/app.js
@@ -1,8 +1,17 @@
+//first we declare a variable to hold our require() node module 'http'
 const http = require('http');
 
+
+//second we declare variables to hold our hostname and port 
 const HOSTNAME= process.env.HOSTNAME || 'localhost';
 const PORT = process.env.PORT || 3000;
 
+
+//third we declare a variable to hold our newly created server
+    //our new server variable must
+    //our server will need a status code 200
+    //our server will need a header with the correct content-type
+    //our server will end and leave a message (message must be in 'content-type' setting)
 const server = http.createServer((request,response)=>{
     response.statusCode=200;
     response.setHeader('content-type','text/plain');
@@ -10,6 +19,7 @@ const server = http.createServer((request,response)=>{
 
 })
 
+//fourth we must listen to our newly created server variable
 server.listen(PORT,HOSTNAME,()=>{
     console.log(`server running at http://${HOSTNAME}:${PORT}/`)
 })
