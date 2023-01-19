27dcb7b3d442c902611b61abba687f4ebaf4bedf
Kevin <kevinalexanderduran8@gmail.com>
2023-01-18 17:26:29 -0800

    test

--- node_sandbox/app.js
+++ node_sandbox/app.js
@@ -1,14 +1,30 @@
-const http = require('http')
 
-const PORT = process.env.PORT || 3002
+// A VERY SIMPLE WEB SERVER
+// const http = require('http')
 
-const server = http.createServer((req,res)=>{
-    res.statusCode=200
-    res.setHeader('Content-Type','text/plain')
-    res.end('hello,friend')
-})
+// const hostname = 'localhost'
+// const port = 3000
 
-server.listen(PORT,()=>{
-    console.log(`you can find your server at ${PORT}/`)
-})
+// const server = http.createServer((req,res)=>{
+//     res.statusCode = 200
+//     res.setHeader('content-type','text/plain')
+//     res.end('anakyn my allegiance is to the republic, to democracy!')
+// })
+
+// server.listen(port,hostname,()=>{
+//     console.log(`server is on: http://${hostname}:${port}`)
+// })
+
+
+// const readline = require('readline');
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout
+// });
+
+// rl.question('What is your name? ', (answer) => {
+//   console.log(`Your name is: ${answer}`);
+//   rl.close();
+// });
+
 
